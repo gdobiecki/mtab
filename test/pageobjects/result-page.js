7438fe977de101b32444fc8d41d93ce0d167007a
grzesiek <g.dobiecki@gmail.com>
2019-06-05 22:17:43 +0200

    first test finished

--- test/pageobjects/result-page.js
+++ test/pageobjects/result-page.js
@@ -1,15 +1,20 @@
 class ResultPage {
     get firstBikePrice() {
-        return $
+        return $("div.opbox-listing--base section article:nth-child(1) div.bf8839e span > span");
     }
 
     get resultCounter() {
-        return $("span[class='listing-title__counter-value']");
+        return $("div[class='listing-title__counter'] span");
     }
 
-    getAmountOfItems() {
+    getNumberOfItemsReturned() {
         this.resultCounter.waitForDisplayed(5000);
         return this.resultCounter.getText();
+    }
+
+    getFirstBikePrice() {
+        this.firstBikePrice.waitForDisplayed(5000);
+        return this.firstBikePrice.getText();
     }
 }
 
--- test/specs/base.spec.js
+++ test/specs/base.spec.js
@@ -5,11 +5,7 @@ import resultPage from '../pageobjects/result-page';
 let assert = require('chai').assert;
 
 describe('allegro test suite', function () {
-    beforeEach(function () {
-        console.log("1234567890")
-    });
-
-    it('find first bike\'s price ', function () {
+    it('find first bike\'s price and number of offers ', function () {
         landingPage.open();
         popupWindow.closePopUp();
         assert.equal(browser.getTitle(), 'Allegro.pl – najlepsze ceny, największy wybór i zawsze bezpieczne zakupy online');
@@ -17,10 +13,17 @@ describe('allegro test suite', function () {
         landingPage.searchForItem('rower')
         landingPage.clickSearchButton();
 
-        const amountOfReturnedItems = resultPage.getAmountOfItems();
-        //Amount of bikes offered may change by the time you review this task.
-        assert.equal(amountOfReturnedItems, '379 467');
+        let firstBikePrice = resultPage.getFirstBikePrice();
+        console.log("First bike's price: " + firstBikePrice);
+        //let's get rid of " zł"
+        firstBikePrice = firstBikePrice.slice(0, -3);
+        //let's replace comma with dot
+        firstBikePrice = firstBikePrice.replace(/,/g, '.');
 
-        // let firstBikePrice = resultPage
+        const numberOfReturnedItems = resultPage.getNumberOfItemsReturned();
+        console.log("Number of offers: " + numberOfReturnedItems);
+
+        let priceInPennies = (firstBikePrice * 100).toFixed(2).slice(0, -3);
+        assert.isTrue(priceInPennies > numberOfReturnedItems, "Price in pennies should be greater than number of offers");
     });
 });
